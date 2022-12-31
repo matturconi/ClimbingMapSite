3e73f51150627b8c22d0e8984a70ead064e65c34
mat029 <mat029@bucknell.edu>
2022-12-31 18:18:50 -0500

    Initial Check in of App

---
+++ database-scripts/CreateClimbingRouteTable.sql
@@ -0,0 +1,12 @@
+-- Active: 1672518843966@@127.0.0.1@3306@climbingroutes
+
+CREATE TABLE IF NOT EXISTS ClimbingRoute(  
+    Id          INT             NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    Name        VARCHAR(256)    NOT NULL,
+    AvgStars    DECIMAL         NOT NULL,
+    RouteType   INT             NOT NULL,       -- Enum val for route type
+    Difficulty  VARCHAR(16)     NOT NULL,
+    Length      INT             NULL,
+    LocationId  INT             NOT NULL,
+    CONSTRAINT FK_ClimbingRoute_LocationId FOREIGN KEY (LocationId) REFERENCES ClimbingArea(Id)
+);
